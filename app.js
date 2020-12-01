d350d696a611cc6e407fcc09b085a6fdb5063c7d
Christopher Lee <leechrs1@gmail.com>
2020-11-30 21:33:15 -0600

    Update app.js
    Fixed problem with overlay not going away.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
-const overlay = document.getelementById('overlay');
-const startGame = document.getElementsById('btn__reset');
+const overlay = document.getElementById('overlay');
+const startGame = document.querySelector('.btn__reset');
 
 let missed = 0;
 
