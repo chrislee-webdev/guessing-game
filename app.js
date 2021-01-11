d00e88d6e0daf149cda9f69b49f8991310caf155
Christopher Lee <leechrs1@gmail.com>
2021-01-10 23:42:36 -0600

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const overlay = document.getElementById('overlay');
 const startGame = document.querySelector('.btn__reset');
+const phraseArray = getRandomPhraseAsArray(phrases);
 
 let missed = 0;
 
@@ -28,8 +29,13 @@ const phrases = [
 
 let getRandomPhraseAsArray = phrases[Math.floor(Math.random() * phrases.length)];
 
-return getRandomPhraseAsArray;
+return getRandomPhraseAsArray(phrases);
 
 //Set Game Display
 
+function addPhraseToDisplay(arr) {
+    for (let i = 0; i <= getRandomPhraseAsArray(phrases).length; i++) {  
+        
+    }
+};
 
